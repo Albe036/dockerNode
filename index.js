82d7f025a8035e9f42532540574eb76606f0c7fa
Albeiro <albeiro036@gmail.com>
2023-01-30 21:06:06 +0000

    first commit in docker

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello world from express!!!Albe");
+});
+
+app.listen(3000, () => {
+  console.log("Listen on port 3000");
+});
